e66f925a7a9c4f682c5c68b8399825f061b060f4
jadc <chehimj@gmail.com>
2023-01-26 14:41:13 -0700

    2023-01-26_14:41:13

--- run_sql.py
+++ run_sql.py
@@ -1,24 +1,15 @@
 import sqlite3, sys, os.path
 
-# Feel free to modify the code in this function.
 def sandbox(db):
-    #ex(db, "INSERT INTO musicPerson VALUES (1600, 'Lil Uzi Vert', 1928)")
-    #ex(db, "INSERT INTO musicPerson VALUES (2900, 'Playboi Carti', 1995)")
-    #ex(db, "INSERT INTO singer VALUES (2900)")
-    ex(db, "INSERT INTO music VALUES ('my song', 2020, 60)")
-    ex(db, "INSERT INTO music VALUES ('new song', 2021, 65)")
-    #ex(db, "INSERT INTO singerOf VALUES (2900, 'rapper', 'my song', 2020)")
-    #ex(db, "DELETE FROM musicPerson WHERE pid = 2900")
-    ex(db, "INSERT INTO genres VALUES ('rock', 'my song', 2020)")
-    ex(db, "INSERT INTO genres VALUES ('rck', 'my song', 2020)")
-    preview(db, 'music')
-    preview(db, 'genres')
+    pass  # Feel free to modify the code in this function.
 
+# e.g. ex(db, "INSERT INTO music VALUES ('my song', 2020, 60)")
 def ex(db, query):
     query = ' '.join(query.split())
     try: return db.execute( query )
     except Exception as e: print(f"!!! Error in: '{query}'\n--> {e.__class__.__name__}: {e}"); exit(1);
 
+# e.g. preview(db, 'music')
 def preview(db, table):
     print( table, ex(db, 'SELECT * FROM ' + table ).fetchall() )
 
