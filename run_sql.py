ea2b40731672f9ac7a185939b9d5baeef838d22c
jadc <chehimj@gmail.com>
2023-02-01 16:15:10 -0700

    2023-02-01_16:15:10

--- run_sql.py
+++ run_sql.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
 import sqlite3, sys
+
+##################
 
 def sandbox(db):
     pass  # Feel free to modify the code in this function.
+
+##################
 
 # e.g. ex(db, "INSERT INTO music VALUES ('my song', 2020, 60)")
 def ex(db, query):
@@ -13,23 +18,23 @@ def ex(db, query):
 def preview(db, table):
     print( table, ex(db, 'SELECT * FROM ' + table ).fetchall() )
 
+args = ['PRAGMA foreign_keys=ON']
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         try:
-            db = sqlite3.connect(":memory:")
-            db = db.cursor()
-            db.execute('PRAGMA foreign_keys=ON')
+            db = sqlite3.connect(":memory:").cursor()
+            for arg in args: db.execute(arg)
 
-            with open(sys.argv[1], 'r') as sql_file:
-                db.executescript(sql_file.read())
-
-            print(f"Created new database from '{sys.argv[1]}'")
-
-            sandbox(db)
+            try:
+                with open(sys.argv[1], 'r') as sql_file:
+                    db.executescript(sql_file.read())
+                print(f"Created new database from '{sys.argv[1]}' in memory")
+                sandbox(db)
+                exit(0)
+            except Exception as e:
+                print(f"Failed to interpret '{sys.argv[1]}' as an SQL file\n{e.__class__.__name__}: {e}", file=sys.stderr)
         except FileNotFoundError as e:
-            print(f"{sys.argv[0]}: cannot access '{sys.argv[1]}': No such file")
-            exit(1)
+            print(f"{sys.argv[0]}: cannot access '{sys.argv[1]}': No such file", file=sys.stderr)
     else:
-        print(f"Usage: python {sys.argv[0]} <*.sql>")
-        exit(1)
-
+        print(f"Usage: {sys.argv[0]} <*.sql>", file=sys.stderr)
+    exit(1)
